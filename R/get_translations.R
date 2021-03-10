b1463485e0576a85d5527d2cd43f771fd907d077
Anthony Durrant <antdurrant@gmail.com>
2021-03-10 12:13:19 +0900

    finesse get_translation and get_wordlist

--- R/get_translations.R
+++ R/get_translations.R
@@ -80,6 +80,42 @@ get_translation <-  function(token, part_of_speech, language = "jpn"){
   }
 }
 
+#' Get English definitions of individual words from nltk
+#'
+#' This will match up to 3 meanings where the requested word and part of speech match.
+#' For more information on synsets, see the
+#' Open Multilingual Wordnet documentation.
+#'
+#' @param token an English word to pass to nltk's synset search
+#' @param part_of_speech one of "v" -verb, "a" - adjective, "n" - noun, or "r" - adverb
+#'
+#' @return a character vector of length one, with definitions separated by ";\n"
+#' @export
+#'
+get_definition <- function(token, part_of_speech){
+
+  if(length(synsets(token, pos = part_of_speech)) == 0) {
+    NA_character_
+  } else{
+    purrr::map(
+      1:length(synsets(token, pos = part_of_speech)) ,
+      ~synsets(token, pos = part_of_speech)[[.x]]$definition()
+    ) %>%
+      # lose anything that did not return a value
+      # return the top four unique entries
+      # lose all results with capitals (they are probably unwanted proper nouns)
+      purrr::discard(is.null) %>%
+      unlist() %>%
+      unique() %>%
+      stringr::str_remove("(?=;).+") %>%
+      dplyr::na_if("") %>%
+      purrr::discard(is.na) %>%
+      utils::head(3) %>%
+      stringr::str_flatten(collapse = ";\n ") %>%
+      stringr::str_replace_all("_", " ")
+  }
+}
+
 
 #' Make wordlists from udpiped dataframes
 #'
@@ -92,11 +128,12 @@ get_translation <-  function(token, part_of_speech, language = "jpn"){
 #'
 #' @param data a dataframe containing doc_id, sentence_id, token_id, token, lemma, upos. Should be piped from udpipe.
 #' @param language three-character abbreviation of language to find translations for. Refer to `wordlists::nltk_languages`.
+#' @param def TRUE will add a definition column, anything else will leave it out. Defaults to TRUE
 #'
 #' @return a dataframe
 #' @export
 #'
-get_wordlist <- function(data, language){
+get_wordlist <- function(data, language, def = TRUE){
 
   pos_trans <- tibble::tribble(
     ~upos, ~pos,
@@ -106,7 +143,7 @@ get_wordlist <- function(data, language){
     "VERB",  "v",
     "ADV",  "r"
   )
-  doc_id <- sentence_id <- token_id <- token <- lemma <- upos <- pos <- NULL
+
   base <- data %>%
     dplyr::select(doc_id, sentence_id, token_id, token, lemma, upos) %>%
     dplyr::left_join(pos_trans)
@@ -117,11 +154,19 @@ get_wordlist <- function(data, language){
   yep <- base %>%
     dplyr::filter(!is.na(pos))
 
-  yep %>%
-    dplyr::mutate(translation = purrr::map2_chr(lemma, pos, ~get_translation(.x, .y, lang = language))) %>%
+  res <- yep %>%
+    dplyr::mutate(translation = purrr::map2_chr(token, pos, ~get_translation(.x, .y, lang = language)))
+
+  if (def == TRUE){
+    res <- res %>%
+      dplyr::mutate(definition = purrr::map2_chr(token, pos, ~get_definition(.x, .y)))
+  }
+
+  res %>%
     dplyr::bind_rows(nope) %>%
     dplyr::mutate(token_id = as.numeric(token_id)) %>%
     dplyr::arrange(doc_id, sentence_id, token_id) %>%
-    dplyr::select(-doc_id, -sentence_id) %>%
     dplyr::filter(upos != "PUNCT")
 }
+
+
