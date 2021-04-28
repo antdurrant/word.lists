a75b6e820b6def599369e97d175d315e6fb60eaf
Anthony Durrant <antdurrant@gmail.com>
2021-04-28 09:37:05 +0900

    add add_off_list_groups function

---
+++ R/add_off_list_groups.R
@@ -0,0 +1,20 @@
+#' Add off-list groups to generated word-lists
+#'
+#' Provides "number", "proper-noun", "pronoun", & "off-list" groupings for words that do not appear in the word lists you are using as a reference.
+#' This should be used after producing wordlists with `word.lists::get_wordlists`, `tidytext::unnest_tokens` etc. AND
+#' joining it to a word list with `lemma` and `group` variables
+#'
+#' @param data
+#'
+#' @return a dataframe with a mutated group variable
+#' @export
+#'
+add_off_list_groups <- function(data){
+  data %>%
+    mutate(group = case_when(is.na(group) & str_detect(lemma, "[0-9]") ~ "NUMBER",
+                             is.na(group) & upos == "PROPN"            ~ "PROPER-NOUN",
+                             is.na(group) & upos == "PRON"             ~ "PRONOUN",
+                             is.na(group)                              ~ "OFF-LIST",
+                             TRUE                                      ~ as.character(group))
+    )
+}
