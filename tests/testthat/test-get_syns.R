f43fd372b5aeac435c1ee9925713be0a3dfcd1f6
Anthony Durrant <antdurrant@gmail.com>
2021-09-29 14:48:27 +0900

    check character coersion

--- tests/testthat/test-get_syns.R
+++ tests/testthat/test-get_syns.R
@@ -13,9 +13,19 @@ test_that("`get_syns` returns a character vector of length 1",{
                expected = NA_character_)
   })
 
+test_that("`get_translation` can handle non-character inputs to token",{
+  expect_equal(
+    object = get_translation(token = 1, part_of_speech = "a", language = "jpn"),
+    expected = ""
+  )
+
+})
+
+
 
 test_that("`get_syns` throws a useful error when a non-language is passed to `language`",{
   expect_error(object = get_translation(token = "missile", part_of_speech =  "v", language = "GERMAN")
                )
 })
+
 
